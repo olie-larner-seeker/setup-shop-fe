657687b91e8a4ec43147b345953d733d544593d2
olie-larner-seeker <olie.larner@seeker.digital>
2020-08-25 10:13:49 +0100

    Add rerouting graphql

--- src/utils/ApolloContext.js
+++ src/utils/ApolloContext.js
@@ -1,6 +1,18 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const uri = () => {
+  if (window.location.hostname === "localhost") {
+    return "http://localhost:8888/Setup_Shop/setup-shop-be/graphql";
+  }
+  if (
+    window.location.hostname ===
+    "https://wordpress-458085-1446350.cloudwaysapps.com/"
+  ) {
+    return "https://wordpress-458085-1446350.cloudwaysapps.com/graphql";
+  }
+};
+
 export const client = new ApolloClient({
-  uri: `http://localhost:8888/Setup_Shop/setup-shop-be/graphql`,
+  uri: uri,
   cache: new InMemoryCache(),
 });
