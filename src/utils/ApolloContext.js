9c11c15275e21b5b95fb7f7b652297a5f5d846ba
olie-larner-seeker <olie.larner@seeker.digital>
2020-08-25 14:05:17 +0100

    amp

--- src/utils/ApolloContext.js
+++ src/utils/ApolloContext.js
@@ -2,16 +2,20 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const uri = () => {
   if (window.location.hostname === "localhost") {
+    console.log(window.location.hostname);
     return "http://localhost:8888/Setup_Shop/setup-shop-be/graphql";
   }
   if (
     window.location.hostname === "https://develop.dldoq5hxs3lte.amplifyapp.com/"
   ) {
+    console.log(window.location.hostname);
     return "https://wordpress-458085-1446350.cloudwaysapps.com/graphql";
   }
 };
 
+console.log(uri());
+
 export const client = new ApolloClient({
-  uri: uri,
+  uri: uri(),
   cache: new InMemoryCache(),
 });
