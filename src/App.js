169861e08e814cf57fd482104c792774f7f55f86
olie-larner-seeker <olie.larner@seeker.digital>
2020-08-26 17:47:12 +0100

    bug fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 import Home from "./Home";
-import Header from "./components/Header";
+import Header from "./components/header";
 import "./styles/global.css";
 import "./styles/tailwind.css";
 
